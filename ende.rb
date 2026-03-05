360d1d20332126c31f1336812f51e957a80bbcff
YoungJinJung <zerone@lguplus.co.kr>
2026-03-05 15:21:54 +0900

    remove stratagy

--- ende.rb
+++ ende.rb
@@ -2,7 +2,7 @@ require_relative "lib/custom_download_strategy"
 class Ende < Formula
   desc "Secure CLI for DevOps teams to share secrets using public-key encryption and sender verification."
   homepage "https://github.com/DevopsArtFactory/ende"
-  url "https://github.com/DevopsArtFactory/ende/releases/download/v0.0.2/ende-darwin-arm64", :using => GitHubPrivateRepositoryReleaseDownloadStrategy # 더미 URL (Homebrew 요구사항 충족용)
+  url "https://github.com/DevopsArtFactory/ende/releases/download/v0.0.2/ende-darwin-arm64"
   version "v0.0.2"
   sha256 "1754678ab0437748d51a9fd2e18545f1dc486a66c35ad8484a4d07c245894126"
 
