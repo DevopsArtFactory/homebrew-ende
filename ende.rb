ae2f5d5536c2ca941ebb9c5384fa0a09e60dfc62
YoungJinJung <zerone@lguplus.co.kr>
2026-04-09 18:09:15 +0900

    update to 0.0.5

--- ende.rb
+++ ende.rb
@@ -1,10 +1,9 @@
-require_relative "lib/custom_download_strategy"
 class Ende < Formula
   desc "Secure CLI for DevOps teams to share secrets using public-key encryption and sender verification."
   homepage "https://github.com/DevopsArtFactory/ende"
-  url "https://github.com/DevopsArtFactory/ende/releases/download/v0.0.3/ende-darwin-arm64"
-  version "v0.0.3"
-  sha256 "415b5348b4116ec5772111aacae9ae38bd0a8b2b2116098baab62c1cd344318e"
+  url "https://github.com/DevopsArtFactory/ende/releases/download/v0.0.5/ende-darwin-arm64"
+  version "v0.0.5"
+  sha256 "c9600a6d665c182194af23eaa85c242aadf8a83a0ecf6263e4c0b84a04d64ca9"
 
   def install
     bin.install "ende-darwin-arm64" => "ende"
